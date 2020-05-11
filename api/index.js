a8d306e6311f4e381b4392235160b8d337a944f9
David <mosqueradavid@outlook.es>
2020-05-11 11:02:05 -0500

    API Documentation

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 const config = require('../config.js')
-const user = require('./components/user/network')
+const user = require('./components/user/network');
+const bodyParser = require('body-parser');
+
+const swaggerUI = require('swagger-ui-express')
 
 const app = express();
+
+
+app.use(bodyParser.json())
+
+const swaggerDoc = require('./swagger.json')
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
 // ROUTING
 app.use('/api/user', user)
